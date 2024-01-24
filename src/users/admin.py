77f7621ac4f84b72c2f4ca0641d97637195bf563
chestnut90 <cowzon90@gmail.com>
2024-01-24 11:28:21 +0900

    Feat: UserProfile 모델 추가 #7
    - 사용자의 profile 데이터를 저장하기 위해서 User와 1:1인 모델 생성
    - user image와 status message를 저장

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from users.models import User
+from users.models import User, UserProfile
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
     pass
