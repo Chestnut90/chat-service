f91022ff58efa2c5926b7cd47efff1583c94030a
chestnut90 <cowzon90@gmail.com>
2024-01-26 18:12:33 +0900

    Feat: 회원가입 api 추가 #8
    - /api/v1/ 최상위 경로
    - /users/signup 으로 회원가입 api 작성

---
+++ src/users/serializers.py
@@ -0,0 +1,18 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from rest_framework.serializers import ModelSerializer
+
+
+class UserSignupSerializer(ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ("username", "password")
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def validate_password(self, value):
+        validate_password(value)
+        return value
+
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(**validated_data)
+        return user
--- src/users/views.py
+++ src/users/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from users.serializers import UserSignupSerializer
+
+
+class SignupAPIView(APIView):
+    def post(self, request):
+        serializer = UserSignupSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
