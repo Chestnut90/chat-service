722d2519d0fe82e25a0cbc64c3cbc574c66621d9
chestnut90 <cowzon90@gmail.com>
2024-01-26 19:06:47 +0900

    Feat: Swagger Page 설정
    - Django Restframework의 Swagger를 위한 설정

--- src/config/urls.py
+++ src/config/urls.py
@@ -16,8 +16,28 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework.permissions import AllowAny
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Chat service",
+        default_version="v1",
+        description="",
+        contact=openapi.Contact(email="cowzon90@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
     path("api/v1/users/", include("users.urls")),
 ]
